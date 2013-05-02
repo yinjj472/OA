e4e8dad187f916cae6fde89fcfb17375d9e33440
yinjj472 <yinjj472@gmail.com>
2013-05-02 19:16:37 +0800

    Windows commit

--- OA/src/main/java/com/yin/oa/web/UserAction.java
+++ OA/src/main/java/com/yin/oa/web/UserAction.java
@@ -1,12 +1,24 @@
 package com.yin.oa.web;
 
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Namespace;
+import org.springframework.context.annotation.Scope;
+
 /**
  * User: yin
  * Date: 13-4-27
  * Time: 下午2:36
  * User web Action
  */
+@Namespace("user")
+@Scope
 public class UserAction {
+
+    @Action("hello")
+    public String hello() {
+        System.out.println("xxxx");
+        return null;
+    }
 
     public String saveUser() {
         return null;
