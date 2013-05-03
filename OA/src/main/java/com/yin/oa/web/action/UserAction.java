b551fc4e21935d6f8f9db040aef51cbe2b34ef49
yinjj472 <yinjj472@gmail.com>
2013-05-03 11:18:14 +0800

    添加Spring Security的依赖，初步配置

--- OA/src/main/java/com/yin/oa/web/action/UserAction.java
+++ OA/src/main/java/com/yin/oa/web/action/UserAction.java
@@ -2,10 +2,14 @@ package com.yin.oa.web.action;
 
 import com.opensymphony.xwork2.ActionSupport;
 import com.yin.oa.service.UserService;
+import org.apache.struts2.ServletActionContext;
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Namespace;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * User: yin
@@ -23,6 +27,16 @@ public class UserAction extends ActionSupport {
     @Action("hello")
     public String hello() {
         userService.hello();
+
+        HttpServletResponse response = ServletActionContext.getResponse();
+        response.setContentType("text/plain;charset=UTF-8");
+        try {
+            response.getWriter().write("In struts2 hello action method.");
+            response.getWriter().flush();
+        } catch (IOException ex) {
+           // ignore
+        }
+
         return null;
     }
 
